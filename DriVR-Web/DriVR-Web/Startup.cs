d8425a5bf7f0ea285864b2b38a4fb94e5bf75349
Inobinky <bartblaak@chello.nl>
2020-05-29 00:11:21 +0200

    Changed default startup controller

--- DriVR-Web/DriVR-Web/Startup.cs
+++ DriVR-Web/DriVR-Web/Startup.cs
@@ -50,7 +50,7 @@ namespace DriVR_Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Question}/{action=Index}/{id?}");
             });
         }
     }
